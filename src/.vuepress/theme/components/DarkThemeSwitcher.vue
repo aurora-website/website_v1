af7362d5a6360b3f45d14af272a85ecb6d252496
marchingon12 <augustthegreat.cool@gmail.com>
2021-03-11 02:12:39 +0100

    a typo in background-color

--- src/.vuepress/theme/components/DarkThemeSwitcher.vue
+++ src/.vuepress/theme/components/DarkThemeSwitcher.vue
@@ -54,7 +54,7 @@ export default {
 }
 label {
   display: block;
-  background-color: trnasparent;
+  background-color: transparent;
   cursor: pointer;
   -webkit-transition: 0.4s ease-in-out;
   transition: 0.4s ease-in-out;
