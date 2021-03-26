b519e60f6105c5f10365372284759f721b574982
marchingon12 <augustthegreat.cool@gmail.com>
2021-03-26 16:51:07 +0100

    fix typo

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -30,7 +30,7 @@ module.exports = {
 		// custom headers for PWA
 		["link", { rel: "manifest", href: "/manifest.json" }],
 		["meta", { name: "theme-color", content: "#b071e3" }],
-		["meta", { name: "mobile-web-app-capable", content : "yes"],
+		["meta", { name: "mobile-web-app-capable", content : "yes"}],
 		["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
 		["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
 		["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.webp" }],
